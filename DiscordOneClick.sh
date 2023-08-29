21cff17ab037fb603be7550c591c031f40a602b3
Errico <134167732+errivi@users.noreply.github.com>
2023-08-29 19:52:24 +0200

    Update DiscordOneClick.sh

--- DiscordOneClick.sh
+++ DiscordOneClick.sh
@@ -1,16 +1,32 @@
+#!/bin/bash
+
+killall Discord
+
+rm -r "/Applications/Discord.app"
+
 clear
-echo "Welcome to the CopyPaste Discord Installer for macOS!\nThis will install/update your Discord app.\nAll the info after this is the output of the commands, don't look at them if you aren't interested.\nMade by errivi.\n\n\n\n\n\n"
-rm /Applications/TemporaryFiles/_dis.dmg
-rmdir /Applications/TemporaryFiles
+
+echo -e "Welcome to the CopyPaste Discord Installer for macOS!\nThis will install/update your Discord app.\nMade by errivi.\n"
+
+if [ -f "/Applications/TemporaryFiles/_dis.dmg" ]; then
+    rm "/Applications/TemporaryFiles/_dis.dmg"
+fi
+
+mkdir -p "/Applications/TemporaryFiles"
+
 osascript -e 'display notification "Sit and relax while we install Discord..." with title "Discord"'
-cd /Applications/
-mkdir TemporaryFiles
-cd ./TemporaryFiles
-curl "https://dl.discordapp.net/apps/osx/0.0.276/Discord.dmg" --output ./_dis.dmg
-hdiutil attach ./_dis.dmg
-cp -R /Volumes/Discord/Discord.app /Applications/
+
+curl -o "/Applications/TemporaryFiles/_dis.dmg" "https://dl.discordapp.net/apps/osx/0.0.276/Discord.dmg"
+
+hdiutil attach "/Applications/TemporaryFiles/_dis.dmg"
+
+cp -R "/Volumes/Discord/Discord.app" "/Applications/"
+
 osascript -e 'display notification "Done. Enjoy chatting" with title "Discord"'
-diskutil unmount /Volumes/Discord
-rm /Applications/TemporaryFiles/_dis.dmg
-rmdir /Applications/TemporaryFiles
-open /Applications/Discord.app
+
+hdiutil detach "/Volumes/Discord"
+
+rm "/Applications/TemporaryFiles/_dis.dmg"
+
+rmdir "/Applications/TemporaryFiles"
+
